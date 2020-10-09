4d0a37edefc87604e5f4243f59eafbeba47aecb9
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-10-08 20:19:12 -0500

    Initial commit

---
+++ src/InputHotkeyBlock/InputHotkeyBlock.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace COM3D2_Plugins
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class InputHotkeyBlock : BaseUnityPlugin
+    {
+        public const string PluginGUID = "deathweasel.com3d2.inputhotkeyblock";
+        public const string PluginName = "InputHotkeyBlock";
+        public const string PluginVersion = "1.0";
+        private static bool UIInputSelected;
+
+        private void Awake() => Harmony.CreateAndPatchAll(typeof(Hooks));
+
+        private void Update()
+        {
+            if (UIInput.selection == null)
+                UIInputSelected = false;
+            //Right click to deselect the UIInput field, which seems to never want to deselect otherwise
+            else if (Input.GetMouseButtonDown(1))
+                Traverse.Create(UIInput.selection).Method("OnSelect", false).GetValue();
+            else
+                UIInputSelected = UIInput.selection.gameObject.activeInHierarchy;
+        }
+
+        /// <summary>
+        /// Called by the hooks, returns false to prevent the original method from running
+        /// </summary>
+        private static bool HotkeyBlock()
+        {
+            //UI elements from some mods
+            if (GUIUtility.keyboardControl > 0)
+                return false;
+
+            //NGUI UIInput
+            if (UIInputSelected)
+                return false;
+
+            return true;
+        }
+
+        private class Hooks
+        {
+            [HarmonyPrefix, HarmonyPatch(typeof(Input), nameof(Input.GetKey), typeof(KeyCode))]
+            internal static bool GetKeyCode() => HotkeyBlock();
+            [HarmonyPrefix, HarmonyPatch(typeof(Input), nameof(Input.GetKey), typeof(string))]
+            internal static bool GetKeyString() => HotkeyBlock();
+            [HarmonyPrefix, HarmonyPatch(typeof(Input), nameof(Input.GetKeyDown), typeof(KeyCode))]
+            internal static bool GetKeyDownCode() => HotkeyBlock();
+            [HarmonyPrefix, HarmonyPatch(typeof(Input), nameof(Input.GetKeyDown), typeof(string))]
+            internal static bool GetKeyDownString() => HotkeyBlock();
+            [HarmonyPrefix, HarmonyPatch(typeof(Input), nameof(Input.GetKeyUp), typeof(KeyCode))]
+            internal static bool GetKeyUpCode() => HotkeyBlock();
+            [HarmonyPrefix, HarmonyPatch(typeof(Input), nameof(Input.GetKeyUp), typeof(string))]
+            internal static bool GetKeyUpString() => HotkeyBlock();
+        }
+    }
+}
---
+++ src/InputHotkeyBlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using static COM3D2_Plugins.InputHotkeyBlock;
+
+[assembly: AssemblyTitle(PluginName)]
+[assembly: AssemblyDescription(PluginName)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InputHotkeyBlock")]
+[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(PluginVersion)]
+[assembly: AssemblyFileVersion(PluginVersion)]
